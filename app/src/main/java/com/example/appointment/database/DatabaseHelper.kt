98a8f2783cf5c4dea0b886706b90af858d2d388b
IjaasMohamed <undyin0@gmail.com>
2023-05-02 04:35:50 -0300

    working with create

--- app/src/main/java/com/example/appointment/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/appointment/database/DatabaseHelper.kt
@@ -3,6 +3,7 @@ package com.example.appointment.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import com.example.appointment.model.AppointmentListModel
 
 class DatabaseHelper (context : Context) : SQLiteOpenHelper (context , DB_NAME, null, DB_VERSION ) {
 
@@ -15,14 +16,34 @@ class DatabaseHelper (context : Context) : SQLiteOpenHelper (context , DB_NAME, 
         private val APPOINTMENT_DETAILS = "appointmentdetails"
     }
 
-    override fun onCreate(db: SQLiteDatabase?) {
+    override fun onCreate(p0: SQLiteDatabase?) {
         val CREATE_TABLE = "CREATE TABLE $TABLE_NAME($ID INTEGER PRIMARY KEY, $APPOINTMENT_NAME TEXT, $APPOINTMENT_DETAILS TEXT )"
-        db?.execSQL(CREATE_TABLE)
+        p0?.execSQL(CREATE_TABLE)
     }
 
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    override fun onUpgrade(p0: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
         val DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
-        db?.execSQL(DROP_TABLE)
-        onCreate(db)
+        p0?.execSQL(DROP_TABLE)
+        onCreate(p0)
+    }
+
+    fun getAllAppointment () : List <AppointmentListModel> {
+        val appointmentlist = ArrayList<AppointmentListModel> ()
+        val db = writableDatabase
+        val selectQuery = "SELECT * FROM $TABLE_NAME"
+        val cursor = db.rawQuery(selectQuery, null) // data is held by cursor
+        if (cursor != null){
+            if (cursor.moveToFirst()){
+                do {
+                    val appointments = AppointmentListModel()
+                    appointments.id = Integer.parseInt(cursor.getString(cursor.getColumnIndex(ID)))
+                    appointments.name = cursor.getString(cursor.getColumnIndex(TABLE_NAME))
+                    appointments.details = cursor.getString(cursor.getColumnIndex(APPOINTMENT_DETAILS))
+                    appointmentlist.add(appointments)
+                } while (cursor.moveToNext())
+            }
+        }
+        cursor.close()
+        return appointmentlist
     }
 }
---
+++ app/src/main/java/com/example/appointment/model/AppointmentListModel.kt
@@ -0,0 +1,7 @@
+package com.example.appointment.model
+
+class AppointmentListModel {
+    var id : Int  = 1
+    var name : String = ""
+    var details : String = ""
+}
